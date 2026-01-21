5c6266e2d20f78fe151ada1820ff0b7e97be55b8
LiamLegge <liamlegge53@gmail.com>
2026-01-21 10:29:46 -0400

    Changed distances to 0.9 and angle to pi/2.2

--- src/eced3901_dt1.cpp
+++ src/eced3901_dt1.cpp
@@ -116,28 +116,28 @@ class SquareRoutine : public rclcpp::Node
 			switch(count_) 
 			{
 			  case 0:
-			    move_distance(1.0);
+			    move_distance(0.9);
 			    break;
 			  case 1:
-			    turn_angle(M_PI/2);		    
+			    turn_angle(M_PI/2.2);		    
 			    break;
 			  case 2:
-			    move_distance(1.0);
+			    move_distance(0.9);
 			    break;
 			  case 3:
-			    turn_angle(M_PI/2);		    
+			    turn_angle(M_PI/2.2);		    
 			    break;
 			  case 4:
-			    move_distance(1.0);
+			    move_distance(0.9);
 			    break;			    
 			  case 5:
-			    turn_angle(M_PI/2);		    
+			    turn_angle(M_PI/2.2);		    
 			    break;
 			  case 6:
-			    move_distance(1.0);
+			    move_distance(0.9);
 			    break;			    
 			  case 7:
-			    turn_angle(M_PI/2);		    
+			    turn_angle(M_PI/2.2);		    
 			    break;  
 			  default:
 			    break;
