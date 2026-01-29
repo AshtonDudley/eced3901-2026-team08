28677faabf60f28ab39ea8b2ca15b85a34b5c4d6
Ashton Dudley <ashtondudley03@gmail.com>
2026-01-28 20:20:32 -0400

    Added error check -> slightly better (using pi/2)

--- src/eced3901_dt1.cpp
+++ src/eced3901_dt1.cpp
@@ -47,7 +47,7 @@ class SquareRoutine : public rclcpp::Node
 		publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
       
 	  	// Create the timer
-	  	timer_ = this->create_wall_timer(100ms, std::bind(&SquareRoutine::timer_callback, this)); 	  
+	  	timer_ = this->create_wall_timer(20ms, std::bind(&SquareRoutine::timer_callback, this)); 	  
 	}
 
   private:
@@ -78,7 +78,8 @@ class SquareRoutine : public rclcpp::Node
 		// Calculate angle travelled from initial
 		th_now = yaw;
 		
-		
+	    double error = wrap_angle(th_target - th_now);
+        
 		// Keep moving if not reached last distance target
 		if (d_now < d_aim)
 		{
@@ -87,10 +88,10 @@ class SquareRoutine : public rclcpp::Node
 			publisher_->publish(msg);		
 		}
 		// Keep turning if not reached last angular target		
-		else if ( abs(wrap_angle(th_now - th_init)) < th_aim)
+		else if (abs(error) > th_tol)
 		{
 			msg.linear.x = 0; 
-			msg.angular.z = th_vel;
+			msg.angular.z = (error > 0) ? th_vel :  -th_vel;
 			publisher_->publish(msg);		
 		}		
 		// If done step, stop
@@ -116,28 +117,28 @@ class SquareRoutine : public rclcpp::Node
 			switch(count_) 
 			{
 			  case 0:
-			    move_distance(0.95); //move the robot forwards
+			    move_distance(1); //move the robot forwards
 			    break;
 			  case 1:
-			    turn_angle(M_PI/2.05);  //turn the robot
+			    turn_angle(M_PI/2);  //turn the robot
 			    break;
 			  case 2:
-			    move_distance(0.95);  //second movement for the robot
+			    move_distance(1);  //second movement for the robot
 			    break;
 			  case 3:
-			    turn_angle(M_PI/2.05); //turn the robot again
+			    turn_angle(M_PI/2); //turn the robot again
 			    break;
 			  case 4:
-			    move_distance(0.95);  //third movement for the robot
+			    move_distance(1);  //third movement for the robot
 			    break;			    
 			  case 5:
-			    turn_angle(M_PI/2.05);  //final turn
+			    turn_angle(M_PI/2);  //final turn
 			    break;
 			  case 6:
-			    move_distance(0.95); //last movement for the robot
+			    move_distance(1); //last movement for the robot
 			    break;			    
 			  case 7:
-			    turn_angle(M_PI/2.05);  //re-orient the robot
+			    turn_angle(M_PI/2);  //re-orient the robot
 			    break;  
 			  default:
 			    break;
@@ -158,8 +159,8 @@ class SquareRoutine : public rclcpp::Node
 	// Set the initial angle as where robot is heading and put new th_aim in place			
 	void turn_angle(double angle)
 	{
-		th_aim = angle;
-		th_init = th_now;
+        th_init = th_now;
+        th_target = wrap_angle(th_init + angle);
 		count_++;		// advance state counter
 		last_state_complete = 0;	
 	}
@@ -190,6 +191,8 @@ class SquareRoutine : public rclcpp::Node
 	double x_now = 0, x_init = 0, y_now = 0, y_init = 0, th_now = 0, th_init = 0;
 	double d_now = 0, d_aim = 0, th_aim = 0;
 	double q_x = 0, q_y = 0, q_z = 0, q_w = 0; 
+    double th_target = 0.0;
+    double th_tol = 0.02;
 	size_t count_ = 0;
 	int last_state_complete = 1;
 };
