42320d45a63781b8968d751f7bb302a375218b7b
Megan Neville <megan.neville8@gmail.com>
2026-03-06 15:24:54 -0400

    updated wp

--- scripts/waypoint_nav_left.py
+++ scripts/waypoint_nav_left.py
@@ -73,6 +73,7 @@ def main():
         #Necessary half way pose
         [2.6, 0.1, 0.0],
         [1.8, 0.6, 0.78],
+        [1.1, 0.134, 0.0],
         #Head back to origin
         [-0.5, -0.5, 0.0]]
 
