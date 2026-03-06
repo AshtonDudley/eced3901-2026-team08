2d75813b8044b601f273c121e2f2926daa0499c9
Megan Neville <megan.neville8@gmail.com>
2026-03-06 13:52:30 -0400

    update wps

--- scripts/waypoint_nav_right.py
+++ scripts/waypoint_nav_right.py
@@ -62,13 +62,18 @@ def main():
     # [ X-pos, Y-pos, Theta-yaw ]
     inspection_route = [
         #Necessary half way poses
-        [1.8, 0.6, 2.36],
-        [2.6, 0.1, 3.14],
+        [1.8, -0.6, 0.78],
+        [2.6, -0.1, 0.0],
+        #Drop off cargo
         #[3.85, 0.6, 1.57],
-        #[2.8, 0.3, 0.0],
-        [3.45, 0.0, 0.0],
-        [1.8, 0.6, 2.36],
-        [0.0, 0.0, 3.14]]
+        #Reset
+        #[2.8, 0.3, 3.14],
+        #Pick up cargo
+        [3.50, 0.0, 3.14],
+        #Necessary half way pose
+        [1.8, -0.6, 0.78],
+        #Head back to origin
+        [-0.5, -0.5, 0.0]]
 
     # Set our demo's initial pose
  #   initial_pose = PoseStamped()
