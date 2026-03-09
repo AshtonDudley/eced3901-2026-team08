91a46e2be9b372eb11027bf7933b2faa2a20bd50
ECED3901 08 <adudley@dal.ca>
2026-03-09 13:40:54 -0300

    wps

--- scripts/waypoint_nav_right.py
+++ scripts/waypoint_nav_right.py
@@ -81,8 +81,8 @@ def main():
     initial_pose = PoseStamped()
     initial_pose.header.frame_id = 'map'
     initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-    initial_pose.pose.position.x = 3.25
-    initial_pose.pose.position.y = 0.5
+    initial_pose.pose.position.x = 3.3
+    initial_pose.pose.position.y = 0.0
     initial_pose.pose.orientation.z = 0.0
     initial_pose.pose.orientation.w = 0.0
     navigator.setInitialPose(initial_pose)
