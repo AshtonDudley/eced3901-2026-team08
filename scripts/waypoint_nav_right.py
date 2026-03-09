68fb58de636e001c0d98706989bcd9b14d289b76
Megan Neville <megan.neville8@gmail.com>
2026-03-09 11:11:49 -0300

    update yaw

--- scripts/waypoint_nav_right.py
+++ scripts/waypoint_nav_right.py
@@ -71,8 +71,8 @@ def main():
         #Pick up cargo
         [3.50, 0.0, 3.14],
         #Necessary half way pose
-        [2.6, -0.1, 0.0],
-        [1.8, -0.6, 0.0],
+        [2.6, -0.1, 3.14],
+        [1.8, -0.6, 3.14],
         #Head back to origin
         [-0.5, -0.5, 0.0]]
 
