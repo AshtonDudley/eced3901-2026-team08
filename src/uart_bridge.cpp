050ac9c55daff54ca029414a0b3693fbf3b8cf94
LiamLegge <liamlegge53@gmail.com>
2026-03-11 20:54:33 -0300

    Turned off node, turned on

--- src/uart_bridge.cpp
+++ src/uart_bridge.cpp
@@ -169,8 +169,8 @@ private:
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
-    //auto node = std::make_shared<SerialBridgeNode>();
-    //rclcpp::spin(node);
+    auto node = std::make_shared<SerialBridgeNode>();
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
