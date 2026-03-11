92a3f77a4cc533f620c6ca6b9f7a0828c4fd3c8a
LiamLegge <liamlegge53@gmail.com>
2026-03-11 11:11:25 -0300

    New Write and Read Functions

--- src/uart_bridge.cpp
+++ src/uart_bridge.cpp
@@ -20,11 +20,14 @@
 
 using namespace std::chrono_literals;
 
-class SerialBridgeNode : public rclcpp::Node {
+class SerialBridgeNode : public rclcpp::Node{
 public:
-    SerialBridgeNode() : Node("serial_bridge_node") {
+    SerialBridgeNode() : Node("serial_bridge_node"){
         publisher_.push_back(this->create_publisher<std_msgs::msg::Int32>("distance0", 10));
         publisher_.push_back(this->create_publisher<std_msgs::msg::Int32>("distance1", 10));
+
+        //subscribe to scan topic
+        scan_sub_ = this->create_subscription<std_msgs::msg::Laser_Scan>("/scan",10,std::bind(&SerialBridgeNode::scan_callback, this, std::placeholders::_1));
 
         // Start Outputting
         RCLCPP_INFO(this->get_logger(), "=============================================");
@@ -36,14 +39,14 @@ public:
         
         serial_port_ = open("/dev/ttyUSB4", O_RDWR | O_NOCTTY | O_NONBLOCK);
 
-        if(serial_port_ < 0) {
+        if(serial_port_ < 0){
             RCLCPP_ERROR(this->get_logger(), "Error %i from open: %s", errno, strerror(errno));
             rclcpp::shutdown();
             return;
         }
 
         struct termios tty{};
-        if(tcgetattr(serial_port_, &tty) != 0) {
+        if(tcgetattr(serial_port_, &tty) != 0){
             RCLCPP_ERROR(this->get_logger(), "Error from tcgetattr: %s", strerror(errno));
         }
 
@@ -75,10 +78,11 @@ public:
         tcflush(serial_port_, TCIFLUSH);
 
         // Ceate timer loop to read serial
-        timer_ = this->create_wall_timer(10ms, std::bind(&SerialBridgeNode::read_serial, this));
+        read_timer_ = this->create_wall_timer(10ms, std::bind(&SerialBridgeNode::read_serial, this));
+        write_timer_ = this->create_wall_timer(200ms, std::bind(&SerialBridgeNode::write_serial, this));
     }
 
-    ~SerialBridgeNode() { 
+    ~SerialBridgeNode(){ 
         if (serial_port_ >= 0){
         close(serial_port_);
         RCLCPP_INFO(this->get_logger(), "Serial Port Closed");
@@ -86,7 +90,7 @@ public:
     }
 
 private:
-    void read_serial() {
+    void read_serial(){
         static std::string buffer;
         char buf[64];
         int n = read(serial_port_, buf, sizeof(buf) - 1);
@@ -129,13 +133,40 @@ private:
             }
         }
     }
+    void write_serial(){
+        if(scan_data_min_ >= 0.0){
+            std::stringstream ss;
+            ss << "[TOPIC] LiDAR:" << std::fixed << std::setprecision(2) << scan_data_min_ << "\n";
+            std::string data = ss.str();
+
+            if(serial_port_ >= 0){
+                ssize_t byte_write = write(serial_port_, data.c_str(), data.size());
+                if(byte_write < 0 && errno != EAGAIN){
+                    RCLCPP_ERROR(this->get_logger(), "Error writing: %s", strerror(errno));
+                }
+            }
+        }
+    }
+
+    float scan_data_min_ = -1.0;
+    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+        float min_dist = msg->range_max;
+        for(auto range : msg->ranges){
+            if(range < min_dist && range > msg->range_min){
+                min_dist = range;
+            }
+        }
+        scan_data_min_ = min_dist;
+    };
 
     std::vector<rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr> publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::TimerBase::SharedPtr read_timer_;
+    rclcpp::TimerBase::SharedPtr write_timer_;
     int serial_port_;
 };
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
     auto node = std::make_shared<SerialBridgeNode>();
     rclcpp::spin(node);
