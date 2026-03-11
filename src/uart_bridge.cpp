f6f8273684c5a586524a8066da3f26f314fef0b5
LiamLegge <liamlegge53@gmail.com>
2026-03-11 12:56:51 -0300

    Changed include file back to laser_scan

--- src/uart_bridge.cpp
+++ src/uart_bridge.cpp
@@ -17,7 +17,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "sensor_msgs/msg/laserscan.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
 
