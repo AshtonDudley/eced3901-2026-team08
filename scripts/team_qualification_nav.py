5af31777175cae505b5d69b2153d91e9d9157e41
ECED3901 08 <adudley@dal.ca>
2026-03-12 14:45:19 -0300

    update yaw

--- scripts/team_qualification_nav.py
+++ scripts/team_qualification_nav.py
@@ -66,8 +66,8 @@ def main():
         #[2.6, 0.1, 0.0],
         #Drop off cargo
         [3.50, 0.0, 3.14],
-        [2.0, 0.6, 0.0], 
-        [0.0, 0.0, 0.0]]
+        [2.0, 0.6, 3.14],
+        [0.0, -0.5, 0.0]]
 
     # Set our demo's initial pose
     # initial_pose = PoseStamped()
