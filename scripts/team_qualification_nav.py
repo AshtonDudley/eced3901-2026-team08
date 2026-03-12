0dd1aeb0cbec21c8fa60a4ba8c5c7738202b844b
ECED3901 08 <adudley@dal.ca>
2026-03-12 10:09:16 -0300

    try less wp

--- scripts/team_qualification_nav.py
+++ scripts/team_qualification_nav.py
@@ -62,8 +62,8 @@ def main():
     # [ X-pos, Y-pos, Theta-yaw ]
     inspection_route = [
         #Necessary half way poses
-        [1.8, 0.6, 0.0],
-        [2.6, 0.1, 0.0],
+        [2.0, 0.6, 0.0],
+        [#2.6, 0.1, 0.0],
         #Drop off cargo
         [3.50, 0.0, 3.14]]
 
