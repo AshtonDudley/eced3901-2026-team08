5b53f5e99bc551ebc50ad230ff1de887ec37fce5
Megan Neville <megan.neville8@gmail.com>
2026-03-16 18:29:53 -0300

    update wp

--- scripts/team_qualification_nav.py
+++ scripts/team_qualification_nav.py
@@ -61,11 +61,18 @@ def main():
     # from either a map or drive and repeat.
     # [ X-pos, Y-pos, Theta-yaw ]
     inspection_route = [
-        #Necessary half way poses
-        [2.0, 0.6, 0.0],
-        #[2.6, 0.1, 0.0],
+        [1.8, 0.6, 0.78],
+        [2.6, 0.1, 0.0],
         #Drop off cargo
-        [3.50, 0.0, 3.14]]
+        [3.85, 0.6, 1.57],
+        #Reset
+        [2.8, 0.3, 3.14],
+        #Pick up cargo
+        [3.45, 0.0, 3.14],
+        #Necessary half way pose
+        [1.8, 0.6, 0.78],
+        #Head back to origin
+        [-0.5, -0.5, 0.0]]
 
     # Set our demo's initial pose
     # initial_pose = PoseStamped()
