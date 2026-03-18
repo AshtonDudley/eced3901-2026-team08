0f817e5aa0fa97559f6d9a8aae0d0dd0a8775eed
Megan Neville <megan.neville8@gmail.com>
2026-03-18 15:10:59 -0300

    fix error

--- scripts/right_open.py
+++ scripts/right_open.py
@@ -64,7 +64,7 @@ def main():
         #Necessary half way poses
         [0.7, 0.0, 1.78],
         [3.2, 0.0, 0.0],
-        [2.8, 0.0, 0.65] 
+        [2.8, 0.0, 0.65],
         [0.0, 0.0, 0.0]]
 
     # Set our demo's initial pose
