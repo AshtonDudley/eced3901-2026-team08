a21196314223096c819082609ae51d6194828a05
LiamLegge <liamlegge53@gmail.com>
2026-03-17 23:00:40 -0300

    Change serial port to ACM0

--- scripts/uart_bridge.py
+++ scripts/uart_bridge.py
@@ -28,7 +28,7 @@ class SerialBridgeNode(Node):
         # Open connection to linux serial device
         try:
             self.serial_port = serial.Serial(
-                port='/dev/ttyUSB4',
+                port='/dev/ttyACM0',    #/dev/ttyUSB4 for USB-Serial adapters
                 baudrate=115200,
                 bytesize=serial.EIGHTBITS,
                 parity=serial.PARITY_NONE,
