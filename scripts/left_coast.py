023b90c943bb526b3a5f7406881fff3d0ba7c5ce
Megan Neville <megan.neville8@gmail.com>
2026-03-18 10:53:07 -0300

    UPDATE LEFT WPS FOR CARGO PICK UP

--- scripts/left_coast.py
+++ scripts/left_coast.py
@@ -62,13 +62,18 @@ def main():
     # [ X-pos, Y-pos, Theta-yaw ]
     inspection_route = [
         #Necessary half way poses
-        [2.0, 0.6, 0.0],
-        #[2.6, 0.1, 0.0],
+        [1.8, 0.6, 0.78],
+        [2.6, 0.1, 0.0],
         #Drop off cargo
-        [3.3, 0.95, 1.78],
-        [3.30, 0.0, 3.14],
-        [2.0, 0.6, 3.14],
-        [0.0, 0.0, 0.0]]
+        [3.85, 0.6, 1.57],
+        #Reset
+        [2.8, 0.3, 3.14],
+        #Pick up cargo
+        [3.45, 0.0, 3.14],
+        #Necessary half way pose
+        [1.8, 0.6, 0.78],
+        #Head back to origin
+        [-0.5, -0.5, 0.0]]
 
     # Set our demo's initial pose
     # initial_pose = PoseStamped()
