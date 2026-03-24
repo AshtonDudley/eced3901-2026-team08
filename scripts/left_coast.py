82afd9b257ca5c47344474a08ee4c25dfb556fdc
Megan Neville <megan.neville8@gmail.com>
2026-03-24 14:59:20 -0400

    update waypoint

--- scripts/left_coast.py
+++ scripts/left_coast.py
@@ -70,7 +70,7 @@ def main():
         #Reset
         [2.8, 0.3, 3.14],
         #Pick up cargo
-        [3.45, -0.2, 3.55],
+        [3.45, -0.2, -3.10],
         #Necessary half way pose
         [1.8, 0.6, 3.14],
         #Head back to origin
